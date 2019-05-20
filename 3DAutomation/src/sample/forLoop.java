5dfadfd767274d2083a4e84ee9a5085d703853f6
vijaykanth865 <myselfvijay6@gmail.com>
2019-05-20 17:13:52 +0530

    Commit 1

---
+++ 3DAutomation/src/sample/forLoop.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class forLoop {
+
+	public static void main(String[] args) {
+		System.out.println("Hi Vijay");
+		
+		
+		for(int i=0;i<1800000000;i=i+3)
+		{
+			System.out.println("Value is "+i);
+		}
+		System.out.println("Loop ended");
+		
+		int age = 18;
+		if(age>=18)
+		{
+			System.out.println("Eligible for Vote");
+		}
+		else
+		{
+			System.out.println("Not Eligible for Vote");
+		}
+	}
+	
+	
+
+}
